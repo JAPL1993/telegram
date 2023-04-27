41c7d8d77ea07e5bde2ffdebed3f5eb0ec1b3949
JAPL1993 <108290394+JAPL1993@users.noreply.github.com>
2023-04-27 15:31:00 -0600

    update

--- src/eventos/evento_beginInsertion.ts
+++ src/eventos/evento_beginInsertion.ts
@@ -1,11 +1,35 @@
-import TelegramBot from "node-telegram-bot-api";
+import TelegramBot, { Message } from "node-telegram-bot-api";
 import { Socket } from "socket.io-client";
+import { chatArray } from "../utilidades/chats-array";
 
-export default (
+export default async (
   socket: Socket,
   bot: TelegramBot,
   groupTelegram: any,
   data: any
 ) => {
-  bot.sendMessage(groupTelegram, `beginInsertion: ${data}`);
+  const index = chatArray.findIndex(
+    (item: any) => item.id_hash == data[0].id_hash
+  );
+  const { id_cart, seller } = data[0];
+  const sellerName =
+    `${seller.name_seller} ${seller.last_name_seller}`.toLocaleUpperCase();
+  try {
+    const message = await bot.editMessageText(
+      `InsertFolio : ${data.id_hash}
+       Seller : ${sellerName}
+       Cart : ${id_cart}
+       Status : Inserting
+       `,
+      {
+        chat_id: chatArray[index].message.chat.id,
+        message_id: chatArray[index].message.message_id,
+      }
+    );
+    chatArray[index].message = message as Message;
+    chatArray[index].cartId = id_cart;
+    chatArray[index].user = seller;
+  } catch (error) {
+    console.log(error);
+  }
 };
